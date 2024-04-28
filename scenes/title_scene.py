cd075859893302f9a50d3e81e6deccb0441a0f18
Dan <onomatopoeicboom@gmail.com>
2024-04-28 09:09:31 -0700

    upd8 title screen

--- scenes/title_scene.py
+++ scenes/title_scene.py
@@ -32,6 +32,9 @@ class TitleScene(SceneBase):
         self.state = state
         self.click_sound = pygame.mixer.Sound("audio/buttonClick.mp3")
         self.game_icon = GameSprite("images/menuIcon.png", (100, 128))
+        background_image = pygame.image.load("images/hackdavis_background.png")
+        scale_ratio = TITLE_RESOLUTION[0] / background_image.get_size()[0]
+        self.background_image = pygame.transform.smoothscale(background_image, (background_image.get_size()[0] * scale_ratio, background_image.get_size()[1] * scale_ratio))
 
         self.buttons = [
             GameButton("images/Play Button.png", (654, 359), lambda: (self.click_sound.play(), self.set_next_scene(get_scene("Game")))),
@@ -56,10 +59,7 @@ class TitleScene(SceneBase):
         pygame.mixer.Sound.set_volume(menumusic, 1)
 
     def render(self, screen: pygame.Surface):
-        background_image = pygame.image.load("images/hackdavis_background.png")
-        scale_ratio = screen.get_size()[0] / background_image.get_size()[0]
-        background_image = pygame.transform.smoothscale(background_image, (background_image.get_size()[0] * scale_ratio, background_image.get_size()[1] * scale_ratio))
-        screen.blit(background_image, (0, 0))
+        screen.blit(self.background_image, (0, 0))
 
         self.game_icon.draw(screen)
 
