2901554775eb841f2b30b4498064f14e734d35d1
LanceB23 <131405958+LanceB23@users.noreply.github.com>
2024-04-27 19:05:32 -0700

    Added function for downloading for shortened videos Added requirements.txt

--- ytdlMoo.py
+++ ytdlMoo.py
@@ -1,15 +1,38 @@
 import yt_dlp as yt
+import os
+import ffmpeg
+from yt_dlp.utils import download_range_func
 
 
-def download_video(video_url, destination_folder):
-    ydl_opts = {'outtmpl': f'{destination_folder}/%(title)s.%(ext)s'}
+def download_video(video_url):
+    ydl_opts = {'outtmpl': 'library/%(title)s.%(ext)s'}
     with yt.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([video_url])
+    # Simple download based on the URL
+
+
+def download_shortened_video(video_url, start_time, end_time):
+    # Takes parameters for how long the video should run
+    yt_opts = {
+        'verbose': True,
+        'download_ranges': yt.utils.download_range_func(None, [(start_time, end_time)]),
+        'force_keyframes_at_cuts': True,
+        'outtmpl': 'library/%(title)s.%(ext)s'
+    }
+
+    with yt.YoutubeDL(yt_opts) as ydl:
         ydl.download([video_url])
 
 
 if __name__ == "__main__":
-    # Replace with your desired video URL and destination folder
+    # Replace with your desired video URL, start time, and end time
     youtube_video_url = "https://www.youtube.com/watch?v=60ItHLz5WEA"
-    output_folder = "./youtube_videos/"
+    start_time = 2  # Accepts decimal value like 2.3
+    end_time = 7
 
-    download_video(youtube_video_url, output_folder)
+    # Create the "library" folder if it doesn't exist
+    os.makedirs("library", exist_ok=True)
+
+    download_shortened_video(youtube_video_url, start_time, end_time)
+
+
