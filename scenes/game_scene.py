30e43d0f5c800e1d5d643367288a2b12e4ab6306
Dan <onomatopoeicboom@gmail.com>
2024-04-28 01:16:45 -0700

    centered double vid

--- scenes/game_scene.py
+++ scenes/game_scene.py
@@ -11,7 +11,7 @@ class PoseFrame:
     def __init__(self, frame: np.ndarray, motion_tracker: BaseTracker):
         self.frame = frame
         self.frame = cv2.rotate(self.frame, cv2.ROTATE_90_COUNTERCLOCKWISE, self.frame)
-        self.pose_positions = motion_tracker.process_image(frame)
+        self.pose_positions = motion_tracker.process_image(self.frame)
         self.raw_frame: np.ndarray = motion_tracker.raw_image
 
     def frame_size(self):
@@ -22,6 +22,13 @@ class PoseFrame:
 
     def surface(self) -> pygame.Surface:
         return pygame.surfarray.make_surface(self.frame)
+
+    def centered_draw_coords(self, screen_dim: tuple[int, int], left: bool) -> tuple[int, int]:
+        if left:
+            return (screen_dim[0] // 2 - self.frame_size()[0], 0)
+        else:
+            return (screen_dim[0] // 2, 0)
+
     
 class GameScene(SceneBase):
     def __init__(self, video_config: GameVideoConfiguration, state: GameState):
@@ -36,18 +43,19 @@ class GameScene(SceneBase):
         pass
     
     def render(self, screen: pygame.Surface):
-        frame_available, camera_frame = self.video_config.camera.read_frame()
+        camera_frame = self.video_config.camera.read_frame()
 
-        if not frame_available:
+        if camera_frame is None:
             return
 
-        frame_available, video_frame = self.video_config.video.read_frame()
+        video_frame = self.video_config.video.read_frame()
 
-        if not frame_available:
+        if video_frame is None:
             return
 
         pose_camera = PoseFrame(camera_frame, self.video_config.motion_tracker)
         pose_video = PoseFrame(video_frame, self.video_config.motion_tracker)
 
         # Display the frame
-        screen.blit(pose_video.surface(), (0, 0))
+        screen.blit(pose_video.surface(), pose_video.centered_draw_coords(screen.get_size(), True))
+        screen.blit(pose_camera.unannotated_surface(), pose_camera.centered_draw_coords(screen.get_size(), False))
